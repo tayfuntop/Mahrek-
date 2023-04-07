3f74bf7717f2402468bf6a089d71a4508622eb9f
Tayfun Top <tayfun111423@gmail.com>
2023-04-07 09:28:04 +0300

    Update to title and left icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,13 +8,10 @@ const Header = () => {
         <div className='sticky top-0 z-50'>
             <div className='h-16 px-10 bg-slate-100 flex justify-between items-center'>
                 <div className='h-16 w-16 flex items-center justify-center hover:scale-105 transition-transform'>
-                    <a target="_blank" href="https://mahrek.com.tr/">
-                        <img className='scale-150 cursor-pointer ' src={MahrekNG} alt="mahrek" />
-                    </a>
+                    
                 </div>
                 <span className='text-2xl text-black font-semibold text-center hidden sm:block'>
-                    Tayfun Top
-                    Case
+                    Tree System
                 </span>
                 <div className='h-16 w-16 -mr-4 hover:scale-105 transition-transform'>
                     <a target="_blank" href="https://github.com/tayfuntop">
